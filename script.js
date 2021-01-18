06ad1427756bb8edc794f3238d1f91f42b4bd2f8
Miriam Olivier <miriamolivier098@gmail.com>
2021-01-18 16:32:15 +0200

    Added Programmes Image Modal to home page

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ const navSlide = () => {
   });
 };
 navSlide();
+
+//Modal functions
+
+let modal = document.getElementById("imgModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
