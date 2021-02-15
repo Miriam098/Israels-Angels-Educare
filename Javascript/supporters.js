421b3762907353700c879340c57dadb788534cca
Miriam Olivier <miriamolivier098@gmail.com>
2021-02-15 17:16:27 +0200

    Added Middle Modal in Home Page and added slideshow in Supporters Page

---
+++ Javascript/supporters.js
@@ -0,0 +1,81 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Nav
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards
+         ${index / 5 + 0.3}s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Get the modal
+var modal = document.getElementById("donateModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("donateBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
--- script.js
+++ script.js
@@ -25,13 +25,38 @@ navSlide();
 
 //Modal functions
 
-let modal = document.getElementById("imgModal");
+let modals = document.getElementsByClassName("modal");
 
 // Get the button that opens the modal
-let btn = document.getElementById("openModal");
+let btn = document.getElementsByClassName("openModal");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+btn[0].onclick = function () {
+  modals[0].style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function () {
+  modals[0].style.display = "none";
+};
+
+btn[1].onclick = function () {
+  modals[1].style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span[1].onclick = function () {
+  modals[1].style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modals) {
+    modals.style.display = "none";
+  }
+};
 
 //GETTING GALLARY MODAL ELEMENTS
 let modalTwo = document.getElementById("gallaryModal");
@@ -43,30 +68,13 @@ let imgBtn = document.getElementById("openGallaryModal");
 let spanTwo = document.getElementsByClassName("closeGallaryModal")[0];
 
 // When the user clicks the button, open the modal
-openModal.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks the button, open the modal
 openGallaryModal.onclick = function () {
   modalTwo.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks on <span> (x), close the modal
 spanTwo.onclick = function () {
   modalTwo.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 };
 
 // When the user clicks anywhere outside of the modal, close it
